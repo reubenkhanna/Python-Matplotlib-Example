ef52ef04266f5cae4b0b60434f91fb5584093c27
Reuben Khanna <utkershreuben.khanna@gmail.com>
2020-02-02 21:55:06 -0500

    Deleted TK files

--- Tk/label.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
- 
-window = Tk()
- 
-
-l1 = Label (window, text="Lambton College Rocks!!", font=("Arial Bold", 50), relief = RAISED)
- 
-l1.grid (column=0, row=0)
--- Tk/toplevel.py
+++
@@ -1,8 +0,0 @@
-# !/usr/bin/python3
-from tkinter import *
-
-root = Tk()
-root.title("hello")
-top = Toplevel()
-top.title("Python")
-top.mainloop()
